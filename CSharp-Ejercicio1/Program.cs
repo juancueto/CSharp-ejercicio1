4e4e5b863f028c0960da521dd075eb24a87d70d0
juancueto <juan.cueto@outlook.com>
2022-09-30 21:46:30 -0500

    Ejercicio 1

---
+++ CSharp-Ejercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//Ejercicio 1
+
+Console.WriteLine("Ingrese su nombre:");
+string? nombre = Console.ReadLine();
+Console.WriteLine($"Nombre: {nombre}");
+
+//Ejercicio 2
+Console.WriteLine(DateTime.Now);
+
+
+
